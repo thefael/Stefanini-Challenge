ad61f2802152e14373b571afd96a1110d7db5e33
Rafael Rodrigues <rafael-rodrigues-@live.com>
2021-06-15 16:46:33 -0300

    Create FetchError, make FetchService use it and make fetchImage method return URLSessionDataTask

--- Stefanini-Challenge/Network/FetchService.swift
+++ Stefanini-Challenge/Network/FetchService.swift
@@ -1,8 +1,8 @@
 import UIKit
 
 protocol FetchServiceType {
-    func fetchData<T: Decodable>(from request: URLRequest?, completion: @escaping ((Result<T, Error>) -> Void))
-    func fetchImage(from url: URL?, completion: @escaping ((Result<UIImage, Error>) -> Void))
+    func fetchData<T: Decodable>(from request: URLRequest?, completion: @escaping ((Result<T, FetchError>) -> Void))
+    func fetchImage(from url: URL?, completion: @escaping ((Result<UIImage, FetchError>) -> Void)) -> URLSessionDataTask?
 }
 
 class FetchService: FetchServiceType {
@@ -14,28 +14,30 @@ class FetchService: FetchServiceType {
         self.decoder = decoder
     }
 
-    func fetchData<T: Decodable>(from request: URLRequest?, completion: @escaping ((Result<T, Error>) -> Void)) {
-        guard let request = request else { return }
-        session.dataTask(with: request) { data, response, error in
+    func fetchData<T: Decodable>(from request: URLRequest?, completion: @escaping ((Result<T, FetchError>) -> Void)) {
+        guard let request = request else { completion(.failure(.invalidRequest)); return }
+        session.dataTask(with: request) { data, _, _ in
             if let data = data {
                 do {
                     let model = try self.decoder.decode(T.self, from: data)
                     completion(.success(model))
-                } catch let error { completion(.failure(error)) }
-            } else if let error = error {
-                completion(.failure(error))
+                } catch { completion(.failure(.failedToDecodeData)) }
+            } else {
+                completion(.failure(.invalidData))
             }
         }.resume()
     }
 
-    func fetchImage(from url: URL?, completion: @escaping ((Result<UIImage, Error>) -> Void)) {
-        guard let url = url else { return }
-        session.dataTask(with: url) { data, _, error in
+    func fetchImage(from url: URL?, completion: @escaping ((Result<UIImage, FetchError>) -> Void)) -> URLSessionDataTask? {
+        guard let url = url else { completion(.failure(.invalidURL)); return nil }
+        let dataTask = session.dataTask(with: url) { data, _, _ in
             if let data = data, let image = UIImage(data: data) {
                 completion(.success(image))
-            } else if let error = error {
-                completion(.failure(error))
+            } else {
+                completion(.failure(.failedToDecodeData))
             }
-        }.resume()
+        }
+        dataTask.resume()
+        return dataTask
     }
 }
