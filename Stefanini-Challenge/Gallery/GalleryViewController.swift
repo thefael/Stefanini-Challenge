de0ff287eb469e2f094f49181905af4a3082327e
Rafael Rodrigues <rafael-rodrigues-@live.com>
2021-06-15 14:42:51 -0300

    Make GalleryViewController conform to GalleryPresentable

--- Stefanini-Challenge/Gallery/GalleryViewController.swift
+++ Stefanini-Challenge/Gallery/GalleryViewController.swift
@@ -1,8 +1,14 @@
 import UIKit
 
+protocol GalleryPresentable: AnyObject {
+    func presentLinks(_ links: [String])
+    func presentError(_ error: Error)
+}
+
 class GalleryViewController: UIViewController {
-    let galleryView = GalleryView()
+    let galleryView = GalleryView(frame: Constants.screen)
     let presenter = GalleryPresenter()
+    let dataSource = CollectionViewDataSource<String, GalleryCell>()
 
     override func loadView() {
         view = galleryView
@@ -11,6 +17,29 @@ class GalleryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Galeria de imagens"
+        presenter.presentable = self
+        galleryView.collectionView.dataSource = dataSource
+        galleryView.collectionView.register(GalleryCell.self, forCellWithReuseIdentifier: String(describing: GalleryCell.self))
+        configureCell()
         presenter.fetchGallery()
+    }
+
+    private func configureCell() {
+        dataSource.configureCell = { url, cell in
+            cell.imageLink = url
+        }
+    }
+}
+
+extension GalleryViewController: GalleryPresentable {
+    func presentLinks(_ links: [String]) {
+        DispatchQueue.main.async {
+            self.dataSource.items = links
+            self.galleryView.collectionView.reloadData()
+        }
+    }
+
+    func presentError(_ error: Error) {
+        print(error)
     }
 }
