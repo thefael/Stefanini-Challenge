4c87aadd64536bb2e62240779e747fd441e153de
Rafael Rodrigues <rafael-rodrigues-@live.com>
2021-06-15 11:30:31 -0300

    Create gallery presenter

--- Stefanini-Challenge/Gallery/GalleryPresenter.swift
+++ Stefanini-Challenge/Gallery/GalleryPresenter.swift
@@ -1,8 +1,20 @@
-//
-//  GalleryPresenter.swift
-//  Stefanini-Challenge
-//
-//  Created by Rafael Rodrigues on 15/06/21.
-//
-
 import Foundation
+
+class GalleryPresenter {
+    let service: FetchServiceType
+
+    init(service: FetchServiceType = FetchService()) {
+        self.service = service
+    }
+
+    func fetchGallery() {
+        service.fetchData(from: Endpoints.galleryRequest()) { (result: Result<GalleryData, Error>) in
+            switch result {
+            case .success(let gallery):
+                gallery.data.forEach { $0.post.forEach { print($0.link) }}
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
